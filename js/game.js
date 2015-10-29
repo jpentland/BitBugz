32c69a35abca99d86b060c306b412aa743f747c8
Joseph Pentland <jpe@opensynergy.com>
2015-10-29 16:07:40 +0100

    game.js: Create more bugs

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 var CANVAS_X = 640*2;
 var CANVAS_Y = 680;
 var FPS = 30;
+var NUM_BUGS = 20;
 
 var context = document.getElementById('canvasId').getContext("2d");
 var start = false;
@@ -30,16 +31,21 @@ var doStuff = function doStuff() {
 	context.canvas.width = CANVAS_X;
 	context.canvas.height= CANVAS_Y;
 
+	for (var i=0; i < NUM_BUGS-1; i++) {
+
+		bugs.push(new Bug("bug1.svg",
+				  Math.random()*CANVAS_X , Math.random()*CANVAS_Y,
+				  50, 60,
+				  function() {onBugReady(NUM_BUGS);}));
+
+		if (Math.random() < 0.5)
+			bugs[i].dir = 1;
+		else
+			bugs[i].dir = -1;
+	}
+
 	bugs.push(new Bug("bug1.svg", 500, 500, 50, 60, redraw));
-	bugs[0].dir = 1;
-	bugs.push(new Bug("bug1.svg", 500, 500, 50, 60));
-	bugs[1].dir = 1;
-
-	bugs.push(new Bug("bug1.svg", 500, 500, 50, 60));
-	bugs[2].dir = 1;
-
-	bugs.push(new Bug("bug1.svg", 500, 500, 50, 60));
-	bugs[3].dir = 1;
+	bugs[NUM_BUGS-1].dir = 1;
 
 }
 
